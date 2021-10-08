345dae8474e68a1d1afc7c5d2134b9632f0eab8c
Tomas Tisocco <tomastisocco97@gmail.com>
2021-10-08 02:00:53 -0300

    update State Managment and Layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Planilla de Calidad',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -27,40 +28,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   late int _counter1;
-  int _counter2 = 0;
-  int _counter3 = 0;
-  int _counter4 = 0;
+  late int _counter2;
+  late int _counter3;
+  late int _counter4;
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
-    _counter1 = 0;  
-  }
-
-  void _updateCount1(i){
-    setState(() {
-      _counter1++;
-    });
-  }
-
-  void _updateCount2(i){
-    setState(() {
-      _counter2++;
-    });
-  }
-
-  void _updateCount3(i){
-    setState(() {
-      _counter3++;
-    });
-  }
-
-  void _updateCount4(i){
-    setState(() {
-      _counter4++;
-    });
+    _counter1 = 0;
+    _counter2 = 0;
+    _counter3 = 0;
+    _counter4 = 0;
   }
 
   @override
@@ -75,81 +54,120 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text('${_counter1 + _counter2 + _counter3 + _counter4}'),
             Expanded(
-              child:Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.max,
+              child: Row(
                 children: <Widget>[
-                  FeelButton(color: Colors.amber, title: 'Cuatro', counter: _counter1, parentAction: _updateCount1,),
-                  FeelButton(color: Colors.grey, title: 'Dos', counter: _counter2, parentAction: _updateCount2,)
+                  Expanded(
+                    child: FeelButton(
+                      color: Colors.amber,
+                      title: 'Uno',
+                      counter: _counter1,
+                      onChanged: incrementCounter
+                    ),
+                  ),
+                  Expanded(
+                    child: FeelButton(
+                      color: Colors.grey,
+                      title: 'Dos',
+                      counter: _counter2,
+                      onChanged: incrementCounter
+                    ),
+                  )
                 ],
-              )
+              ),
             ),
             Expanded(
-              child:Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.max,
+              child: Row(
                 children: <Widget>[
-                  FeelButton(color: Colors.blueGrey, title: 'Tres', counter: _counter3, parentAction: _updateCount3,),
-                  FeelButton(color: Colors.brown, title: 'Cuatro', counter: _counter4, parentAction: _updateCount4,)
+                  Expanded(
+                    child: FeelButton(
+                      color: Colors.blueGrey,
+                      title: 'Tres',
+                      counter: _counter3,
+                      onChanged: incrementCounter
+                    ),
+                  ),
+                  Expanded( 
+                    child: FeelButton(
+                      color: Colors.brown,
+                      title: 'Cuatro',
+                      counter: _counter4,
+                      onChanged: incrementCounter
+                    ),
+                  ),
                 ],
               )
-            ),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           print('$_counter1');
         },
       ),
     );
   }
+
+  void incrementCounter(String title) {
+    setState(() {
+      _counter1 = (title == 'Uno') ? (_counter1 + 1) : _counter1;
+      _counter2 = (title == 'Dos') ? (_counter2 + 1) : _counter2;
+      _counter3 = (title == 'Tres') ? (_counter3 + 1) : _counter3;
+      _counter4 = (title == 'Cuatro') ? (_counter4 + 1) : _counter4;
+    });
+  }
 }
 
-class FeelButton extends StatefulWidget {
+class FeelButton extends StatelessWidget {
   final Color color;
   final String title;
   final int counter;
-  final ValueSetter<int> parentAction;
+  final onChanged;
 
-  FeelButton({Key? key, required this.color, required this.title, required this.counter, required this.parentAction}) : super(key: key);
-
-  @override
-  _FeelButtonState createState() => _FeelButtonState();
-}
-
-class _FeelButtonState extends State<FeelButton> {
-
-  _FeelButtonState();
+  const FeelButton(
+      {Key? key,
+      required this.color,
+      required this.counter,
+      required this.title,
+      required this.onChanged})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: widget.color,
-      width: MediaQuery.of(context).size.width/2,
+      color: color,
       child: Column(
         children: <Widget>[
-          Text(widget.title,
-          style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold,),
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           Expanded(
-            child:MaterialButton(
-              minWidth: MediaQuery.of(context).size.width,
+            child: MaterialButton(
               elevation: 0.0,
-              child: Text('${widget.counter}', 
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                )
-              ),
-              onPressed: (){
-                widget.parentAction(widget.counter);
+              minWidth: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Text('$counter',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  )),
+              onPressed: () {
+                _onChanged();
               },
             ),
           ),
         ],
       ),
     );
+  }
+
+  _onChanged() {
+    onChanged(title);
   }
 }
